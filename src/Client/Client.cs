4ff28786860a26151df7802915f06337ea0572c7
Marius Bughiu <marius.bughiu@uipath.com>
2023-03-08 22:32:20 +0200

    Add test server + client

---
+++ src/Client/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+var pipe = new NamedPipeClientStream(".", "test", PipeDirection.InOut, PipeOptions.Asynchronous);
+
+await pipe.ConnectAsync();
+
+var bytes = new byte[3000];
+
+while (true)
+{
+    pipe.Write(bytes);
+    await Task.Delay(TimeSpan.FromSeconds(20));
+}
---
+++ src/Server/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Pipes;
+
+var server = new NamedPipeServerStream("test", PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances,
+                    PipeTransmissionMode.Byte, PipeOptions.Asynchronous, inBufferSize: 0, outBufferSize: 0);
+
+await server.WaitForConnectionAsync();
+
+var bytes = new byte[1024];
+int count;
+
+while ((count = await server.ReadAsync(bytes)) > 0)
+{
+    Console.WriteLine($"Received {count} bytes.");
+}
